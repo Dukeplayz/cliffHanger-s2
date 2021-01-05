1cf5b9af337cc472a8619a1ae1d90687832bcb37
Dukeplayz <61636768+Dukeplayz@users.noreply.github.com>
2021-01-05 19:00:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var ball;
+var database, position;
+var form,player,game
+var gameState = 0;
+var playerCount
+
+var allPlayers;
+
+function setup(){
+    database = firebase.database();
+
+    createCanvas(500,500);
+    game = new Game()
+    game.getState()
+    game.start()
+    
+}
+
+function draw(){
+    if (playerCount === 4 ){
+        game.update()
+    }
+    if (gameState === 1){
+        clear()
+        game.play()
+    }
+}
+
